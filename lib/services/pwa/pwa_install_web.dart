0143d2f1dc5529f896360a38794d087479b986ad
tonis2 <tanton@paysure.solutions>
2026-01-25 20:52:08 +0200

    Fix PWA install

--- lib/services/pwa/pwa_install_web.dart
+++ lib/services/pwa/pwa_install_web.dart
@@ -14,6 +14,12 @@ extension type InstallPromptResult(JSObject _) implements JSObject {
   external String get outcome;
 }
 
+/// Extension to access deferredPwaPrompt on window
+extension WindowPwaPrompt on web.Window {
+  external JSObject? get deferredPwaPrompt;
+  external set deferredPwaPrompt(JSObject? value);
+}
+
 /// Web implementation of PwaInstallService
 class PwaInstallService {
   static final PwaInstallService _instance = PwaInstallService._internal();
@@ -27,7 +33,15 @@ class PwaInstallService {
   bool get isInstallAvailable => _deferredPrompt != null;
 
   void init() {
-    print("init");
+    // Check if the prompt was already captured in JavaScript before Flutter loaded
+    final preCapture = web.window.deferredPwaPrompt;
+    if (preCapture != null) {
+      _deferredPrompt = BeforeInstallPromptEvent(preCapture);
+      _installAvailableController.add(true);
+      // Clear it from window so we don't reuse it
+      web.window.deferredPwaPrompt = null;
+    }
+
     web.window.addEventListener(
       'beforeinstallprompt',
       ((web.Event event) {
